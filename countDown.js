1620de4921faec3fc12fe967dc7a97c873162db4
satheeshkumar-a <79327834+satheeshkumar-a@users.noreply.github.com>
2021-04-10 10:04:26 +0530

    Add files via upload

---
+++ countDown.js
@@ -0,0 +1,88 @@
+ var counter=10;
+//     setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter;
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-1;
+//         }, 2000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-2;
+//         }, 3000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-3;
+//         }, 4000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-4;
+//         }, 5000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-5;
+//         }, 6000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-6;
+//         }, 7000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-7;
+//         }, 8000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-8;
+//         }, 9000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-9;
+//         }, 10000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML=counter-10;
+//         }, 11000);
+//         setTimeout(() => {
+//         id=document.getElementById("count");
+//         id.innerHTML="HAPPY BIRTHDAY";
+//         }, 12000);
+//     }, 1000);
+
+setTimeout(() => {
+id=document.getElementById("count");
+id.innerHTML=counter;
+setTimeout(() => {
+id.innerHTML=counter--; 
+setTimeout(() => {
+id.innerHTML=counter--; 
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML=counter--;
+setTimeout(() => {
+id.innerHTML="Happy Independence Day";
+}, 1000);
+}, 1000); 
+}, 1000);
+}, 1000);
+}, 1000); 
+}, 1000);
+}, 1000);                        
+}, 1000);                    
+}, 1000);    
+}, 1000);   
+}, 1000);
+}, 1000);
+   
